806fc984d8c1d023d35dc853da5b65ae35dacde5
bol <bol@kalibol.shakur>
2023-09-25 08:28:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 // Write your solution here!
+
+
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+}
+
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+
+function appendCat(name) {
+  const newArray = [...cats, name];
+  return newArray;
+}
+
+/
+function prependCat(name) {
+  const newArray = [name, ...cats];
+  return newArray;
+}
+
+
+function removeLastCat() {
+  const newArray = cats.slice(0, -1);
+  return newArray;
+}
+
+
+function removeFirstCat() {
+  const newArray = cats.slice(1);
+  return newArray;
+}
+
+
+module.exports = {
+  cats,
+  destructivelyAppendCat,
+  destructivelyPrependCat,
+  destructivelyRemoveLastCat,
+  destructivelyRemoveFirstCat,
+  appendCat,
+  prependCat,
+  removeLastCat,
+  removeFirstCat,
+};
